429540124848e892eceb9e92577bd185ac6e9a32
Dmitry <dmitry.votchitsev@gmail.com>
2023-01-19 21:00:02 +0200

    setup database

--- coconut_cloud/settings.py
+++ coconut_cloud/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'coconut_cloud.cloud',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'coconut_cloud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cloud_database',
+        'USER': 'cloud_admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'POSRT': '',
     }
 }
 
