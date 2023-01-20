28b6b2e70b380b14f5bdb39bfedc338f0231bd55
Dmitry <dmitry.votchitsev@gmail.com>
2023-01-20 23:05:00 +0200

    auth models.py

--- coconut_cloud/cloud/models.py
+++ coconut_cloud/cloud/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, username, password, **extra_fields):
+        if not email:
+            raise ValueError('No email specified')
+
+        if not username:
+            raise ValueError('No username specified')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            **extra_fields
+        )
+
+        user.set_password(password)
+
+        user.save(using = self._db)
+
+        return user
+    
+    def create_user(self, email, username, password):
+        return self._create_user(email, username, password)
+
+    def create_superuser(self, email, username, password):
+        return self._create_user(email, username, password, is_staff = True, is_superuser = True)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.AutoField(primary_key = True, unique = True)
+    username = models.CharField(max_length = 50, unique = True)
+    email = models.EmailField(max_length = 100, unique = True)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
