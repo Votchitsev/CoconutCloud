97ece17d0d85f90107437a5222fd9be0390aa7f0
Dmitry <dmitry.votchitsev@gmail.com>
2023-01-20 23:06:57 +0200

    auth views, serializers

---
+++ coconut_cloud/cloud/views/admin_interface_views/user_view.py
@@ -0,0 +1,31 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from coconut_cloud.cloud.models import User
+from coconut_cloud.cloud.serializers.user_serializer import RegistrUserSerializer
+
+
+class RegistrUserView(CreateAPIView):
+    queryset = User.objects.all()
+
+    serializer_class = RegistrUserSerializer
+
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = RegistrUserSerializer(data = request.data)
+
+        data = {}
+
+        if serializer.is_valid():
+            serializer.save()
+
+            data['response'] = True
+
+            return Response(data, status = status.HTTP_200_OK)
+        
+        else:
+            data = serializer.errors
+
+            return Response(data)
--- coconut_cloud/urls.py
+++ coconut_cloud/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+
+from coconut_cloud.cloud.views.admin_interface_views.user_view import RegistrUserView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('registr/', RegistrUserView.as_view(), name = 'registr')
 ]
