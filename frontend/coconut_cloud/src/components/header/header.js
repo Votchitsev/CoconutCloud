595556761b2403fab9693a7d6c3b2288f6e91cc0
Dmitry <dmitry.votchitsev@gmail.com>
2023-01-30 19:52:53 +0200

    add logo

---
+++ frontend/coconut_cloud/src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+function Header (props) {
+  return (
+    <section className="header">
+      <div className='header--logo'>CoconutCloud</div>
+      <div className='header--menu-container'></div>
+    </section>
+  )
+}
+
+export default Header
