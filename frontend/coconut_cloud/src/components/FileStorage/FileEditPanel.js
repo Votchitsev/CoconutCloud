ecd0ce3c97a22966710a0c52c754abe696ef8b3f
Dmitry <dmitry.votchitsev@gmail.com>
2023-02-25 18:44:59 +0200

    get download link

--- frontend/coconut_cloud/src/components/FileStorage/FileEditPanel.js
+++ frontend/coconut_cloud/src/components/FileStorage/FileEditPanel.js
@@ -3,17 +3,20 @@ import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
 import FileRenameForm from '../forms/fileRenameForm/_FileRenameForm'
 import DeleteFileSubmitForm from '../forms/submitForm/SubmitForm'
+import GetLinkForm from '../forms/getLinkForm/getLinkForm'
 import { downloadFile, getDownloadLink } from '../../api/requests'
 import './FileEditPanel.css'
 import ChangeCommentForm from '../forms/changeCommentForm/changeCommentForm'
+import BASE_URL from '../../config'
 
 function FileEditPanel ({ currentFile, setCurrentFile, setFiles }) {
   const [patchForm, setPatchForm] = useState()
+  const [downloadLink, setDownloadLink] = useState()
   const token = useSelector(state => state.auth.authToken)
 
   const onClickHandler = (action) => {
     if (action === 'download') {
-      const getLink = async () => {
+      const downloadFileHandler = async () => {
         const response = await getDownloadLink(token, currentFile.id)
         const data = await response.json()
 
@@ -31,6 +34,18 @@ function FileEditPanel ({ currentFile, setCurrentFile, setFiles }) {
         setCurrentFile()
       }
 
+      downloadFileHandler()
+    }
+
+    if (action === 'getLink') {
+      const getLink = async () => {
+        const response = await getDownloadLink(token, currentFile.id)
+        const data = await response.json()
+
+        const link = `${BASE_URL}link/${data.link}/`
+        setDownloadLink(link)
+      }
+
       getLink()
     }
 
@@ -43,11 +58,13 @@ function FileEditPanel ({ currentFile, setCurrentFile, setFiles }) {
         <div className='file-edit-panel--item' onClick={ () => onClickHandler('rename')}>Rename</div>
         <div className='file-edit-panel--item' onClick={ () => onClickHandler('changeComment')}>Change comment</div>
         <div className='file-edit-panel--item' onClick={ () => onClickHandler('download')}>Download</div>
+        <div className='file-edit-panel--item' onClick={ () => onClickHandler('getLink')}>Get download link</div>
         <div className='file-edit-panel--item' onClick={ () => onClickHandler('delete')}>Delete</div>
       </div>
       { patchForm === 'rename' ? <FileRenameForm currentFile={ currentFile } token={ token } setForm={ setPatchForm } setFiles={ setFiles } /> : null }
       { patchForm === 'changeComment' ? <ChangeCommentForm currentFile={ currentFile } token={ token } setForm={ setPatchForm } setFiles={ setFiles } /> : null }
       { patchForm === 'delete' ? <DeleteFileSubmitForm currentFile={ currentFile } token={ token } setForm={ setPatchForm } setFiles={ setFiles } /> : null }
+      { patchForm === 'getLink' ? <GetLinkForm link={ downloadLink } setForm={ setPatchForm } /> : null }
     </>
   )
 }
