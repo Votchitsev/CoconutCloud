518669f20bc145546c29f51a498dcab1ea0a24be
Dmitry <dmitry.votchitsev@gmail.com>
2023-03-02 10:18:34 +0200

    make start page and redirect to my-storage

--- frontend/coconut_cloud/src/router.js
+++ frontend/coconut_cloud/src/router.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {
   BrowserRouter as Router, Route, Routes
 } from 'react-router-dom'
+import StartPage from './components/StartPage/StartPage'
 import Header from './components/header/Header'
 import SignUpForm from './components/forms/signUpForm/SignUpForm'
 import SignInForm from './components/forms/signInForm/SignInForm'
@@ -13,6 +14,7 @@ function App () {
     <Router>
       <Header />
       <Routes>
+        <Route path='/' element={<StartPage />} />
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/sign-in' element={<SignInForm />} />
         <Route path='/admin' element={<AdminPanel />} />
